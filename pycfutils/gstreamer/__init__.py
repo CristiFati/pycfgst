0e42a074e81e465b8b9ae2585c4ca96df926e2dd
CristiFati <fati_utcluj@yahoo.com>
2026-06-02 18:09:53 +0300

    feature: pipeline parser (WiP)

--- pycfutils/gstreamer/__init__.py
+++ pycfutils/gstreamer/__init__.py
@@ -1,10 +1,1 @@
-import sys
-
-from .registry_access import RegistryAccess
-
-__all__ = ("RegistryAccess",)
-
-
-if __name__ == "__main__":
-    print("This script is not meant to be run directly.\n")
-    sys.exit(-1)
+# @TODO - cfati: Dummy file - dir is picked up by setuptools.find_packages
--- pycfutils/tests/test_gstreamer.py
+++ pycfutils/tests/test_gstreamer.py
@@ -1,16 +1,24 @@
+import os
 import unittest
 
 try:
-    from pycfutils.gstreamer import RegistryAccess
-except Exception:
-    RegistryAccess = None
+    pcfgst = True
+    import gi
+
+    gi.require_version("Gst", "1.0")
+    from gi.repository import Gst
+
+    from pycfutils.gstreamer.pipeline_parser import PipelineParser
+    from pycfutils.gstreamer.registry_access import RegistryAccess
+except:
+    pcfgst = None
 
 
 # @TODO - cfati: Dummy
-class GStreamerTestCase(unittest.TestCase):
-    if RegistryAccess is None:  # Mock (PyGObject not installed)
+class _GStreamerBaseTestCase(unittest.TestCase):
+    if pcfgst is None:  # Mock (PyGObject not installed)
 
-        def test_registry_access(self):
+        def setUp(self):
             import sys
             from types import ModuleType
 
@@ -35,13 +43,74 @@ class GStreamerTestCase(unittest.TestCase):
             gi.require_version = lambda ns, ver: None
             sys.modules["gi.repository"] = gi.repository
 
-            from pycfutils.gstreamer import RegistryAccess
+    else:
+
+        def setUp(self):
+            Gst.init(argv=None)
+
+
+class GStreamerTestCase(_GStreamerBaseTestCase):
+
+    @classmethod
+    def generate_pipeline(cls, command):
+        return Gst.parse_launch(command)
+
+    @classmethod
+    def normalize_command(cls, command):
+        return command
+
+    @classmethod
+    def read_pipelines(cls):
+        pipelines = []
+        data_file = os.path.join(
+            os.path.dirname(__file__), "data", "gst_launch_pipelines.txt"
+        )
+        with open(data_file) as f:
+            lines = f.readlines()
+        current = []
+        for line in lines:
+            stripped = line.strip()
+            if not stripped or stripped.startswith("#"):
+                if current:
+                    pipelines.append(" ".join(current))
+                    current = []
+                continue
+            if stripped.endswith("\\"):
+                stripped = stripped[:-1].rstrip()
+            if stripped.startswith("gst-launch"):
+                continue
+            current.append(stripped)
+        if current:
+            pipelines.append(" ".join(current))
+        return pipelines
+
+    def test_registry_access(self):
+        global RegistryAccess
+        if pcfgst is None:
+
+            from pycfutils.gstreamer.registry_access import RegistryAccess
 
             ra = RegistryAccess()
             self.assertIsNotNone(ra)
+        else:
 
-    else:  # Real test (although dummy)
-
-        def test_registry_access(self):
             ra = RegistryAccess()
             self.assertIsInstance(ra.contents(), dict)
+
+    def test_pipeline_parser(self):
+        global PipelineParser
+        if pcfgst is None:
+
+            from pycfutils.gstreamer.pipeline_parser import PipelineParser
+
+            pparser = PipelineParser()
+            self.assertIsNotNone(pparser)
+        else:
+
+            pparser = PipelineParser()
+            pipeline_strings = tuple(self.read_pipelines())
+            for idx, pipeline_string in enumerate(pipeline_strings[:2]):
+                print(f"----- Pipeline {idx}:\n--- Input:\n{pipeline_string}\n")
+                pipeline = self.generate_pipeline(pipeline_string)
+                output = pparser.gst_launch(pipeline)
+                print(f"--- Output:\n{output}\n")
